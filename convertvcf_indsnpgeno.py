24e2dd664326d0a9c9b5fae480ffc5cd879077b9
Skyler He <skylerhe@MacBook-Pro.local>
2024-05-31 20:56:41 -0400

    Updated version

--- convertvcf_indsnpgeno.py
+++ convertvcf_indsnpgeno.py
@@ -8,64 +8,45 @@ __copyright__ = 'Copyright 2022'
 __credits__ = 'Melinda Yang Lab, University of Richmond'
 __maintainer__ = 'Skyler He'
  
-def convertvcf_indsnpgeno(vcf,ind,indcsv,snp,geno):
-    indcsv.write('Individual_name' + ',' + 'Gender' + ',' + 'Population_name'+'\n')
+def convertvcf_indsnpgeno(vcf:str,
+    ind:object,
+    indcsv:object,
+    snp:object,
+    geno:object) -> None:
+    
+    COMMA = ","
+    TAB = "\t"
+    NL = "\n"
+    columns = ('Individual_name', 'Gender', 'Population_name')
+    indcsv.write(COMMA,join(columns) + NL)
     #lighter way to read lines in vcf file
     for line in vcf:
         #split lines into lists by '\n'
         aList = line.decode().split()
         #find the background information
-        if aList[0][:2] == '##':
+        if aList[0].startswith('##'):
             readme = aList
+
         #find the header
-        elif aList[0][:1] == '#':
+        elif aList[0].startswith('#'):
             for indname in aList[9:]:
-                ind.write(indname+'\t'+'U'+'\t'+'Unknown_population'+'\n')
-                indcsv.write(indname+','+''+','+''+'\n')
+		ind.write(TAB.join([indname, 'U', 'Unknown_population'] + NL)
+		indcsv.write(COMMA.join(indname,",")) + NL)
             #write individual name into ind file
 
         #find the database
-        elif aList[0] == 'Scaffold_1' or aList[0] == 'Scaffold_3' or aList[0] == 'Scaffold_4' or aList[0] == 'Scaffold_5':
+        elif aList[0] in ('Scaffold_1', 'Scaffold_3', 'Scaffold_4', 'Scaffold_5'):
             data = aList
-            chronum = data[0]
-            position = data[1]
-            snpID = data[2]
-            ref = data[3]
-            alt = data[4]
+            chronum, posiiton, snpID, ref, alt = data[:5]
             genoinfo = data[9:]
-            snp.write(snpID +'\t'+ chronum[-1:] +'\t'+ '0' +'\t'+ position+ '\t' +ref+'\t'+alt+'\n')
+            snp.write(TAB.join([snpID, chronum[-1:], '0', position, ref, alt]) + NL)
 
             #create genotable in different conditions
             genotable = '' 
             for gt in genoinfo:
-                # two reference alleles
-                if gt[0:3] == '0/0':
-                    genotable += '2'
-                elif gt[0:3] == '0|0':
-                    genotable += '2'
-                # one reference alleles & one alt 
-                elif gt[0:3] == '0/1':
-                    genotable += '1'
-                elif gt[0:3] == '0|1':
-                    genotable += '1'
-                # one alt & one ref
-                elif gt[0:3] == '1/0':
-                    genotable += '1'
-                elif gt[0:3] == '1|0':
-                    genotable += '1'
-                 #two alt alleles
-                elif gt[0:3] == '1/1':
-                    genotable += '0'
-                elif gt[0:3] == '1|1':
-                    genotable += '0'
-                #missing data
-                elif gt[0:3] == './.':
-                    genotable += '9'
-                elif gt[0:3] == '.|.':
-                    genotable += '9'
-                elif gt[0] == '.':
-                    genotable += '9'
-
+                datum = gt[0:3]
+		# two reference alleles
+		genotable += str(9 if '.' in datum else datum.count('0'))
 
             #write to geno file        
             geno.write(genotable + '\n')
